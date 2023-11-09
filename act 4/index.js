d3c67cb47a5f2839ebbf6d463d660307c701652c
russ <117883052+Russel-0@users.noreply.github.com>
2023-11-09 15:45:40 +0800

    save

--- act 4/index.js
+++ act 4/index.js
@@ -6,7 +6,7 @@ var acc = [
     },
     {
         username:"russel1",
-        password:"calderon1"
+        password:"calderon"
     }
 ]
 
@@ -15,9 +15,10 @@ function getData() {
     var username = document.getElementById("txtEmail").value
     var password = document.getElementById("txtPass").value
 
-    for(i=1; i<acc.length; i++){
+    for(i=0; i<acc.length; i++){
         if(username == acc[i].username && password == acc[i].password){
-            console.log("Login Connected")
+            alert("Login Connected")
+            window.location.href="../act4/act3/index.html"
         }
     }
 }
