e747645ac6953de362cb13a617f0a2b681ee246f
russ <117883052+Russel-0@users.noreply.github.com>
2023-11-13 19:27:32 +0800

    update js

--- act 4/index.js
+++ act 4/index.js
@@ -14,16 +14,18 @@ function getData() {
 
     var username = document.getElementById("txtEmail").value
     var password = document.getElementById("txtPass").value
-
+    var found = false
+    
     for(i=0; i<acc.length; i++){
         if(username == acc[i].username && password == acc[i].password){
             alert("Login Connected")
             window.location.href="/act 4/act3/index.html"
+            found = true
             break;
         }
     }
 
-    if(username != acc[i].username && password != acc[i].password){
+    if(!found){
         alert("Access Denied")
     }
 }
