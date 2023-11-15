fcf06d00f4ca7a0bd4c57954264020e873e425dc
russ <117883052+Russel-0@users.noreply.github.com>
2023-11-15 10:58:43 +0800

    save

--- act 4/index.js
+++ act 4/index.js
@@ -1,56 +1,47 @@
 
-
 function login() {
-
+    
     var acc = [
         {
-            username:"russel",
-            password:"calderon"
+            username: "russel",
+            password: "calderon"
         },
         {
-            username:"russel1",
-            password:"calderon1"
+            username: "russel1",
+            password: "calderon1"
         },
         {
-            username:"russel2",
-            password:"calderon2"
+            username: "russel2",
+            password: "calderon2"
         }
-    ]
+    ];
 
-    var username = document.getElementById("txtEmail").value
-    var password = document.getElementById("txtPass").value
-    var found = false
-    
-    for(var i=0; i<acc.length; i++){
-        if(username == acc[i].username && password == acc[i].password){
-            alert("Login Connected")
-            window.location.href="/act 4/act3/index.html"
-            found = true
-            break
+    var username = document.getElementById("txtEmail").value;
+    var password = document.getElementById("txtPass").value;
+    var found = false;
+
+    for (var i = 0; i < acc.length; i++) {
+        if (username == acc[i].username && password == acc[i].password) {
+            alert("Login Connected");
+            window.location.href = "/act 4/act3/index.html";
+            found = true;
+            break;
         }
+    }
 
-        if(!found){
-            alert("Access Denied")
-            document.getElementById("w").style.display = "block";
-            document.getElementById("w").style.color = "red";
-    
-            if(username != acc[i].username){
+    if (!found) {
+        alert("Access Denied");
+        document.getElementById("w").style.display = "block";
+
+        for (var i = 0; i < acc.length; i++) {
+            if (username != acc[i].username) {
                 document.getElementById("txtEmail").style.borderBottomColor = "red";
-                break
-            }else if(password != acc[i].password){
+            } else if (password != acc[i].password) {
                 document.getElementById("txtPass").style.borderBottomColor = "red";
-                break
-            }else {
+            } else {
                 document.getElementById("txtEmail").style.borderBottomColor = "red";
                 document.getElementById("txtPass").style.borderBottomColor = "red";
-                break
             }
         }
     }
-}
-
-function signup() {
-
-    var password = document.getElementById("txtPass").value
-
 }
